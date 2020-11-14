c76a9622a55e340cfaed8071224f12add999bd7d
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2020-11-15 09:55:15 +1100

    Final Version of App including README

---
+++ README.md
@@ -0,0 +1,44 @@
+
+# README for Employee Tracker CMS
+          
+## Description 
+              
+This employee tracker CMS uses a MySQL database and a Node JS interface to allow the user to input and select data relevant to create a fully functional employee tracker for a business.
+        
+## Table of Contents
+* Title
+* Description
+* Installation
+* Useage
+* License
+* Contributing
+* Tests
+* Questions
+
+    
+## Installation
+To install this application, the user runs the “server.js” file in Node. Running the server file will connect to the MySQL database and prompt the user for a series of questions relating to the creation of their CMS . The employee_tracker DB has been populated with seed data, but requires the user to build the CMS by answering the relevant prompts. , User answers are compiled in MySQL commands which write the new data, update the existing data or delete the data as determined by the user. Various functions are used to combine data from tables depending on which options the user chooses. 
+
+## Usage 
+The application requires two main Node packages : inquirer, fs and mySQL. Those packages are used together to prompt the user for information and create the database calls/commands. Connections to the MySQL database we made many times throughout the application, depending on the data required by the user. Step by step instructions can be seen within the attached Screencastify video. https://youtu.be/CW5j4agOJjs.
+(Screenshots to be added once README created)
+
+## Credits
+This was a solo project, however the project relied heavily on Node JS and the packages mentioned above. I sought advice from my class instructors, the Ask BCS instructors and would like to thank Renato for his assistance during our Sunday night tutoring sessions.
+
+## License
+MIT
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Contributing
+Should others wish to contribute to this application, I thank them for their interest and request that they use the standards from at the Contributor Covenant. 
+Note: the [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard
+
+## Tests
+There were no tests developed for this application.
+
+## Questions
+Please direct questions to Please direct questions to Andrewmiddleton1, https://github.com/andrewmiddleton1
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+
     port: 3306,
 
     // Your username
@@ -26,7 +26,7 @@ connection.connect(function (err) {
 
 // function which prompts the user for what action they should take
 function start() {
-    connection.query("SELECT * FROM employee", function (err, results) {
+    connection.query("SELECT * FROM employee", function (err, employeeList) {
         if (err) throw err;
         //console.log(results);
 
@@ -35,7 +35,7 @@ function start() {
                 name: "initialchoice",
                 type: "list",
                 message: "Would you like to do?",
-                choices: ["View all employees", "View all employees by Department", "View all employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View all roles"]
+                choices: ["View all employees", "View all employees by Department", "View all employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View all roles", "Add Department", "Add Role", "Delete Department", "Delete Role", "View Department Budget", "Finish Entering Data"]
             })
             .then(function (answer) {
                 // based on their answer, either call the bid or the post functions
@@ -43,7 +43,7 @@ function start() {
                 if (answer.initialchoice === "View all employees") {
                     viewAllEmployees();
                     function viewAllEmployees() {
-                        console.table(results)
+                        console.table(employeeList)
 
                         start();
                     }
@@ -89,6 +89,49 @@ function start() {
                         });
                     }
                 }
+
+                else if (answer.initialchoice === "View all employees by Department") {
+                    viewAllEmployeesByDepartment();
+                    function viewAllEmployeesByDepartment() {
+                        connection.query("SELECT * FROM department", function (err, results) {
+                            if (err) throw err;
+                            inquirer
+                                .prompt([
+
+                                    {
+                                        name: "department",
+                                        type: "list",
+                                        choices: function () {
+
+                                            return results.map((department) => ({
+                                                name: department.name
+
+                                            }));
+
+                                        },
+
+
+                                        message: "Please select the department you wish to view"
+                                    }
+                                ])
+                                .then(function (answer) {
+                                    var query = "SELECT employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary";
+                                    query += " FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) WHERE (department.name = ?) ORDER BY employee.last_name";
+
+                                    //console.log(query);
+                                    //console.log(answer.department);
+
+                                    connection.query(query, [answer.department], function (err, res) {
+                                        console.table(res);
+
+                                    });
+
+                                    start();
+                                });
+                        });
+                    }
+                }
+
                 else if (answer.initialchoice === "View all employees by Manager") {
                     viewAllEmployeesByManager();
 
@@ -102,7 +145,7 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((manager) => ({
+                                        return employeeList.map((manager) => ({
                                             name: manager.last_name,
                                             value: manager.id
                                         }))
@@ -197,7 +240,7 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((manager) => ({
+                                        return employeeList.map((manager) => ({
                                             name: manager.last_name,
                                             value: manager.id
                                         }))
@@ -242,7 +285,7 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((employee) => ({
+                                        return employeeList.map((employee) => ({
                                             name: employee.last_name
 
                                         }))
@@ -253,7 +296,7 @@ function start() {
 
                             ])
                             .then(function (answer) {
-                                var query = "SELECT employee FROM employee WHERE (employee.last_name = ?)";
+                                var query = "DELETE FROM employee WHERE (employee.last_name = ?)";
 
                                 //console.log(query);
                                 //console.log(answer.department);
@@ -283,7 +326,7 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((employee) => ({
+                                        return employeeList.map((employee) => ({
                                             name: employee.last_name
 
                                         }))
@@ -364,7 +407,7 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((employee) => ({
+                                        return employeeList.map((employee) => ({
                                             name: employee.last_name
 
                                         }))
@@ -377,13 +420,11 @@ function start() {
                                     type: "list",
                                     choices: function () {
 
-                                        return results.map((manager) => ({
+                                        return employeeList.map((manager) => ({
                                             name: manager.last_name,
                                             value: manager.id
                                         }))
-                                        // var managerArray = [];
-                                        // for (var i = 0; i < results.length; i++) {
-                                        //     managerArray.push(results[i].last_name);
+
                                     },
 
 
@@ -391,13 +432,13 @@ function start() {
                                 }
                             ])
                             .then(function (answer) {
-                                console.log(answer.newManger);
+                                console.log(answer.newManager);
                                 console.log(answer.secondNameUpdateManager);
                                 connection.query(
                                     "UPDATE employee SET ? WHERE ?",
                                     [
                                         {
-                                            manager_id: answer.newManger
+                                            manager_id: answer.newManager
                                         },
                                         {
                                             last_name: answer.secondNameUpdateManager
@@ -410,13 +451,6 @@ function start() {
                                     }
                                 );
 
-                                //console.log("Your employee role was successfully updated! ");
-                                // function (err) {
-                                //     if (err) throw err;
-                                //     console.log("Your entry was created successfully!");
-                                //     // re-prompt the user for if they want to bid or post
-                                //     start();
-                                // };
 
                             });
 
@@ -424,6 +458,221 @@ function start() {
 
                     };
                 }
+
+                // "View all roles"
+                else if (answer.initialchoice === "View all roles") {
+                    viewAllRoles();
+                    function viewAllRoles() {
+                        connection.query("SELECT * FROM role", function (err, results) {
+                            console.table(results);
+                            start();
+                        });
+                    };
+                }
+
+                //"Add Department", 
+                else if (answer.initialchoice === "Add Department") {
+                    addDepartment();
+                    function addDepartment() {
+
+                        inquirer
+                            .prompt([
+
+                                {
+                                    name: "newDepartment",
+                                    type: "input",
+                                    message: "Please enter the name of the new department"
+                                }
+                            ])
+                            .then(function (answer) {
+                                connection.query(
+                                    "INSERT INTO department SET ?",
+                                    {
+                                        name: answer.newDepartment
+
+                                    },
+
+                                );
+                                console.log("You successfully created a new department");
+                                start();
+                            });
+
+
+
+                    };
+                }
+
+                //"Add Role", 
+                else if (answer.initialchoice === "Add Role") {
+                    addRole();
+                    function addRole() {
+
+                        inquirer
+                            .prompt([
+
+                                {
+                                    name: "newRole",
+                                    type: "input",
+                                    message: "Please enter the name of the new role"
+                                }
+                            ])
+                            .then(function (answer) {
+                                connection.query(
+                                    "INSERT INTO role SET ?",
+                                    {
+                                        title: answer.newRole
+
+                                    },
+
+                                );
+                                console.log("You successfully created a new role");
+                                start();
+                            });
+
+
+
+                    };
+                }
+                //"Delete Department", 
+                else if (answer.initialchoice === "Delete Department") {
+                    deleteDepartment();
+                    function deleteDepartment() {
+                        connection.query("SELECT * FROM department", function (err, results) {
+                            if (err) throw err;
+                            inquirer
+                                .prompt([
+
+                                    {
+                                        name: "deleteDepartment",
+                                        type: "list",
+                                        choices: function () {
+
+                                            return results.map((department) => ({
+                                                name: department.name
+
+                                            }));
+
+                                        },
+
+
+                                        message: "Please select the department you wish to delete"
+                                    }
+                                ])
+                                .then(function (answer) {
+                                    var query = "DELETE FROM department WHERE (department.name = ?)";
+
+
+                                    connection.query(query, [answer.deleteDepartment], function (err, res) {
+                                        console.log("You successfully deleted: " + answer.deleteDepartment);
+                                        start();
+
+                                    });
+                                });
+
+                        });
+
+                    };
+                }
+                //"Delete Role", 
+                else if (answer.initialchoice === "Delete Role") {
+                    deleteRole();
+                    function deleteRole() {
+                        connection.query("SELECT * FROM role", function (err, results) {
+                            if (err) throw err;
+                            inquirer
+                                .prompt([
+
+                                    {
+                                        name: "deleteRole",
+                                        type: "list",
+                                        choices: function () {
+
+                                            return results.map((role) => ({
+                                                name: role.title
+
+                                            }));
+
+                                        },
+
+
+                                        message: "Please select the role you wish to delete"
+                                    }
+                                ])
+                                .then(function (answer) {
+                                    var query = "DELETE FROM role WHERE (role.name = ?)";
+
+
+                                    connection.query(query, [answer.deleteRole], function (err, res) {
+                                        console.log("You successfully deleted: " + answer.deleteRole);
+                                        start();
+
+                                    });
+                                });
+
+                        });
+
+                    };
+                }
+
+                //"View Department Budget", 
+                else if (answer.initialchoice === "View Department Budget") {
+                    viewDepartmentBudget();
+                    function viewDepartmentBudget() {
+                        connection.query("SELECT * FROM department", function (err, results) {
+                            if (err) throw err;
+                            inquirer
+                                .prompt([
+
+                                    {
+                                        name: "viewDepartmentForBudget",
+                                        type: "list",
+                                        choices: function () {
+
+                                            return results.map((department) => ({
+                                                name: department.name,
+                                                // value: department.id
+
+
+                                            }));
+
+                                        },
+
+
+                                        message: "Please select the department for which you would like the budget"
+                                    }
+                                ])
+                                .then(function (answer) {
+                                    var query = "SELECT employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.salary";
+                                    query += " FROM employee INNER JOIN role ON (employee.role_id = role.id) INNER JOIN department ON (role.department_id = department.id) WHERE (department.name = ?) ORDER BY employee.last_name";
+
+                                    //console.log(query);
+                                    //console.log(answer.manager);
+
+                                    connection.query(query, [answer.viewDepartmentForBudget], function (err, res) {
+
+
+                                        console.table(res);
+
+
+                                        var totalSalary = 0;
+                                        for (var i = 0; i < res.length; i++) {
+                                            totalSalary = totalSalary + (res[i].salary);
+                                        }
+
+                                        // console.log(totalSalary);
+                                        console.log("The total budget for " + answer.viewDepartmentForBudget + " department is: $" + totalSalary);
+
+                                    });
+
+                                    start();
+                                });
+
+                        });
+                    };
+                }
+
+                else console.log("Thankyou for using this CMS");
+                //process.exit(1);
 
 
 
